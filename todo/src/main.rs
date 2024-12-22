da1d476b42638a3e2dfd3a7e9f4cc42ddb1c84a9
agastya gaur <agastyagaur17@gmail.com>
2024-12-22 18:42:42 +0530

    add todo for rust

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 struct TodoItem {
+    id: u32,
     name: String,
     completed: bool,
 }
@@ -19,11 +20,46 @@ fn main(){
     io::stdin().read_line(&mut choice).expect("Failed to read line");
 
     match choice.trim() {
-        "1"=>{}
+        "1"=>{
+            let mut name = String::new();
+            println!("Name of the to-do item:");
+            io::stdin().read_line(&mut name).expect("Failed to read line");
+            let id = (todo.len() + 1) as u32;  
+            let it = TodoItem{
+                id,
+                name:name.trim().to_string(),
+                completed: false,
+            };
+            todo.push(it);
+            },
+        "2"=>{
+            let mut id= String::new();
+            println!("Enter the id of the to-do item you want to complete:");
+            io::stdin().read_line(&mut id).expect("Failed to read line");
+            let id: u32 = id.trim().parse().expect("Invalid input");
+            if let Some(item)=todo.iter_mut().find(|item| item.id == id){
+
+                item.completed = true;
+                println!("To-do item {} completed", id);
+
+            }else{
+                println!("No item found with ID {}", id);
+            }
+        },
+        "3"=>{
+            for i in  &todo{
+                println!("{} - {} - {}", i.id, i.name, i.completed);
+            }
+        }, 
+
+        "4"=>
+            break,
+        _=> println!("Invalid input"),
+        
+        }
         
     }
    }
 
    
 
-}
