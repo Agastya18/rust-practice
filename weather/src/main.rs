bc2c3dbc7cd354009343382f825429880a90c184
agastya gaur <agastyagaur17@gmail.com>
2024-12-24 00:18:33 +0530

    add weather ap rusty

---
+++ weather/src/main.rs
@@ -0,0 +1,45 @@
+
+use std::io;
+use reqwest::blocking::get;
+use serde::Deserialize; 
+use colored::*;
+
+#[derive(Deserialize)]
+struct WeatherResponse {
+    main: Main,
+    weather: Vec<Weather>,
+    name: String,
+}
+#[derive(Deserialize)]
+struct Main {
+    temp: f32,
+    pressure: u32,
+    humidity: u32,
+}
+
+
+#[derive(Deserialize)]
+struct Weather {
+    description: String,
+}
+fn main() {
+  println!(" welcome to the weather application");
+  let mut city = String::new();
+
+  println!("enter the city name:");
+  io::stdin().read_line(&mut city).expect("Failed to read line"); 
+
+  let api = "67c83b1e5fd973ba5633e44d42558ce3";
+
+  let url = format!("http://api.openweathermap.org/data/2.5/weather?q={},&appid={}&units=metric", city.trim(),api);
+
+  let response:WeatherResponse =get(&url).expect("Failed to get response").json().expect("Failed to parse json");
+  println!(
+    "{} in {}: {}°C, {}",
+    response.weather[0].description.green(), // Colorize weather description in green
+    response.name.blue(), // Colorize city name in blue
+    response.main.temp, // Display temperature in Celsius
+    response.main.humidity // Display humidity percentage
+);
+}
+
